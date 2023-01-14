f9287678c878718892fddfad746d722373c8b692
Dan Goodman <dan@danthegoodman.com>
2023-01-14 08:37:15 -0500

    upload and download s3 file

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,14 +1,35 @@
 import S3 from 'aws-sdk/clients/s3'
 import fs from 'fs'
+import { logger, logMsgKey } from '../logger'
+import util from 'util'
+import stream from 'stream'
+
+const pipeline = util.promisify(stream.pipeline)
 
 const s3 = new S3({
   endpoint: process.env.S3_ENDPOINT ? process.env.S3_ENDPOINT : undefined
 })
 
 export async function UploadS3File(fileName: string, readStream: fs.ReadStream) {
-  return s3.upload({
+  await s3.upload({
     Bucket: process.env.S3_BUCKET!,
     Key: fileName,
     Body: readStream
   }).promise()
+  logger.debug({
+    [logMsgKey]: "uploaded s3 file",
+    fileName
+  })
+}
+
+export async function DownloadS3File(fileName: string, writeStream: fs.WriteStream) {
+  const readStream = s3.getObject({
+    Bucket: process.env.S3_BUCKET!,
+    Key: fileName,
+  }).createReadStream()
+  await pipeline(readStream, writeStream)
+  logger.debug({
+    [logMsgKey]: "downloaded s3 file",
+    fileName
+  })
 }
