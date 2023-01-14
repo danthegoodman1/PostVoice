17e7f4d6470c7ba3709c6a345dbe3a01bedc57ff
Dan Goodman <dan@danthegoodman.com>
2023-01-14 11:52:10 -0500

    add voices

---
+++ src/synth/index.ts
@@ -0,0 +1,26 @@
+export const voices = {
+  neural: [
+    "en-US-Wavenet-A",
+    "en-US-Neural2-B",
+    "en-US-Neural2-C",
+    "en-US-Neural2-D",
+    "en-US-Neural2-E",
+    "en-US-Neural2-F",
+    "en-US-Neural2-G",
+    "en-US-Neural2-H",
+    "en-US-Neural2-I",
+    "en-US-Neural2-J",
+  ],
+  wavenet: [
+    "en-US-Wavenet-A",
+    "en-US-Wavenet-B",
+    "en-US-Wavenet-C",
+    "en-US-Wavenet-D",
+    "en-US-Wavenet-E",
+    "en-US-Wavenet-F",
+    "en-US-Wavenet-G",
+    "en-US-Wavenet-H",
+    "en-US-Wavenet-I",
+    "en-US-Wavenet-J",
+  ]
+}
