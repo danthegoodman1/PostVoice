e46b702b2761786af198ea45d5e2342768b522bf
Dan Goodman <dan@danthegoodman.com>
2023-01-15 07:40:17 -0500

    comment

--- migrations/20230113154124-init.sql
+++ migrations/20230113154124-init.sql
@@ -50,7 +50,7 @@ CREATE TABLE synthesis_jobs (
 
   chars INT8 NOT NULL,
   ms INT8 NOT NULL,
-  job TEXT NOT NULL, -- The job that was done, such as webflow/{site_id}/{item id}
+  job TEXT NOT NULL, -- The job that was done, such as webflow/{site_id}/{slug} or ghost/{site_id}/{post_id}
 
   PRIMARY KEY(user_id, id)
 )
