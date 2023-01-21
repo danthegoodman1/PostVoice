a7e46c29a120e2dc55a6d2b53940f63f69298680
Dan Goodman <dan@danthegoodman.com>
2023-01-20 20:32:55 -0500

    separate handlers
    create separate router for webhooks

--- src/index.ts
+++ src/index.ts
@@ -15,16 +15,11 @@ import cors from 'cors'
 
 import { logger } from "./logger"
 import { ConnectDB } from "./db"
-import { HandleWebflowCollectionItemCreation, inngest, CreateWebflowSite, HandleWebflowDeleteItem } from "./inngest"
-import { decrypt, encrypt } from "./utils/crypto"
-import { GetSiteByID } from "./db/queries/sites"
-import WHHandler from "./clerk/wh_handlers"
-import { InvalidWebhookAuth } from "./clerk/errors"
+import { HandleWebflowCollectionItemCreation, CreateWebflowSite, HandleWebflowDeleteItem } from "./inngest"
 import { HandleListSites } from "./sites"
-import { randomID } from "./utils/id"
-import { ListAvailableWebflowSitesForTokens, ListCollectionsForSite } from "./webflow/sites"
-import { GetWebflowToken, InsertWebflowAccessToken } from "./db/queries/webflow"
-import { RowsNotFound } from "./db/errors"
+
+import * as WebflowHandlers from "./handlers/webflow"
+import * as WebhookHandlers from "./handlers/webhook"
 
 declare global {
   namespace Express {
@@ -33,8 +28,6 @@ declare global {
 }
 
 const listenPort = process.env.PORT || "8080"
-
-const webflow = new Webflow()
 
 async function main() {
   const app = express()
@@ -83,149 +76,22 @@ async function main() {
 
   // Webflow endpoints
   const webflowRouter = express.Router()
-  webflowRouter.get("/authorize", (req, res) => {
-    const url = webflow.authorizeUrl({
-      client_id: process.env.WEBFLOW_CLIENT_ID!,
-      redirect_uri: process.env.API_URL + "/webflow/token"
-    })
-    res.json({url})
-  })
-  webflowRouter.get("/token", ClerkExpressRequireAuth(), async (req: Request<{}, {}, {}, {code: string}>, res: Response) => {
-    const { access_token } = await webflow.accessToken({
-      client_id: process.env.WEBFLOW_CLIENT_ID!,
-      client_secret: process.env.WEBFLOW_CLIENT_SECRET!,
-      code: req.query.code,
-      redirect_uri: process.env.API_URL + "/webflow/token"
-    })
-
-    await InsertWebflowAccessToken({
-      id: randomID("wftok_"),
-      access_token: encrypt(access_token),
-      user_id: req.auth.userId
-    })
-
-    console.log('token', access_token)
-    const wf = new Webflow({ token: access_token });
-    const { user } = await wf.authenticatedUser()
-    console.log(user)
-    const sites = await wf.sites()
-    console.log(sites)
-    res.redirect(`${process.env.FE_URL}/sites?action=add_webflow`)
-  })
-  webflowRouter.post("/wh/:siteID/:event", async (req: Request<{siteID: string, event: string}, {}, {}>, res) => {
-    console.log('got webhook event', req.params.event, req.body)
-    const site = await GetSiteByID(req.params.siteID)
-    switch (req.params.event) {
-      case "collection_item_created":
-        await inngest.send("api/webflow.collection_item_created", {
-          data: {
-            whPayload: req.body,
-            siteID: req.params.siteID,
-            encWfToken: site.access_token
-          }
-        })
-        logger.debug('sent inngest event')
-        break;
-      case "collection_item_changed":
-        await inngest.send("api/webflow.collection_item_changed", {
-          data: {
-            whPayload: req.body,
-            siteID: req.params.siteID,
-            encWfToken: site.access_token
-          }
-        })
-        break;
-
-      default:
-        break;
-    }
-
-    res.sendStatus(200)
-  })
-  webflowRouter.get("/sites", ClerkExpressRequireAuth(), async (req, res) => {
-    try {
-      const sites = await ListAvailableWebflowSitesForTokens(req.auth.userId)
-      return res.json({
-        sites
-      })
-    } catch (error) {
-      logger.error(error, "error getting sites")
-      return res.sendStatus(500)
-    }
-  })
-  webflowRouter.get("/sites/:siteID/collections", ClerkExpressRequireAuth(), async (req: Request<{siteID: string}, {}, {}, {tokenID: string}>, res: Response) => {
-    try {
-      const collections = await ListCollectionsForSite(req.auth.userId, req.params.siteID, req.query.tokenID)
-      return res.json({
-        collections
-      })
-    } catch (error) {
-      if (error instanceof RowsNotFound) {
-        return res.status(404).send("token not found")
-      }
-      logger.error(error, "error getting collections")
-      return res.sendStatus(500)
-    }
-  })
-  webflowRouter.post("/sites/add", ClerkExpressRequireAuth(), async (req: Request<{}, {}, { tokenID: string, siteID: string, collectionID: string }>, res: Response) => {
-    try {
-      const token = await GetWebflowToken(req.auth.userId, req.body.tokenID)
-      const wf = new Webflow({
-        token: decrypt(token.access_token)
-      })
-
-      const site = await wf.site({
-        siteId: req.body.siteID
-      })
-
-      const collection = await wf.collection({
-        collectionId: req.body.collectionID
-      })
-
-      const siteID = randomID("site_")
-
-      await inngest.send("api/webflow.create_site", {
-        data: {
-          whPayload: req.body,
-          site: site,
-          collection: collection,
-          siteID,
-          encWfToken: token.access_token
-        },
-        user: {
-          id: req.auth.userId
-        }
-      })
-      return res.json({
-        siteID
-      })
-    } catch (error) {
-      logger.error(error, "error getting sites")
-      return res.sendStatus(500)
-    }
-  })
+  webflowRouter.get("/authorize", )
+  webflowRouter.get("/token", ClerkExpressRequireAuth(), WebflowHandlers.GetToken)
+  webflowRouter.get("/sites", ClerkExpressRequireAuth(), WebflowHandlers.GetSites)
+  webflowRouter.get("/sites/:siteID/collections", ClerkExpressRequireAuth(), WebflowHandlers.GetSiteCollections)
+  webflowRouter.post("/sites/add", ClerkExpressRequireAuth(), WebflowHandlers.PostAddSite)
   app.use("/webflow", webflowRouter)
-
-  const clerkRouter = express.Router()
-  clerkRouter.post("/wh", (req, res) => {
-    try {
-      return WHHandler(req, res)
-    } catch (error) {
-      if (error instanceof InvalidWebhookAuth) {
-        return res.sendStatus(401)
-      }
-      logger.error({
-        error
-      }, "error handling clerk webhook")
-      return res.sendStatus(500)
-    }
-  })
-  app.use("/clerk", clerkRouter)
 
   const siteRouter = express.Router()
   siteRouter.use(ClerkExpressRequireAuth())
   siteRouter.get("/", HandleListSites)
   app.use("/sites", siteRouter)
+
+  const webhookHandler = express.Router()
+  webhookHandler.post("/webflow/:siteID/:event", WebhookHandlers.HandleWebflowSiteEvent)
+  webhookHandler.post("/clerk", WebhookHandlers.HandleClerkEvent)
+  app.use("/wh", webhookHandler)
 
   const server = app.listen(listenPort, () => {
     logger.info(`API listening on port ${listenPort}`)
