e7f1b027c5a83bfdd783a49b6c3ce25802157055
Dan Goodman <dan@danthegoodman.com>
2023-01-22 17:23:14 -0500

    comment

--- src/db/queries/synthesis_jobs.ts
+++ src/db/queries/synthesis_jobs.ts
@@ -5,6 +5,7 @@ export async function InsertSynthesisJob(params: SynthesisJob) {
   return await pool.query(`INSERT INTO synthesis_jobs (user_id, id, ms, chars, site_id, post_slug, audio_path) VALUES ($1, $2, $3, $4, $5, $6, $7)`, [params.user_id, params.id, params.ms, params.chars, params.site_id, params.post_slug, params.audio_path])
 }
 
+// TODO: Need index (or using ksuid?)
 export async function ListSynthesisJobs(userID: string, offset?: Date) {
   const params: any[] = [userID]
   if (offset) {
